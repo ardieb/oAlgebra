7e2841b2f61fcc4553191fe45238c38ede7f2789
imp25 <imp25@cornell.edu>
2019-04-10 12:13:37 -0400

    Operation tests

--- a6-src/_build/test.ml
+++ a6-src/_build/test.ml
@@ -15,8 +15,12 @@ let make_op_test
 
 let rationals_tests =
   let add = RATIONAL.add in
+  let mul = RATIONAL.mul in
+  let div = RATIONAL.div in
+  let sub = RATIONAL.sub in
   [
     make_op_test "Add - 0 + 0 = 0" add (0,1) (0,1) (0,1);
+    make_op_test "Add - x + 0 = x" add (1,1) (0,10) (1,1);
     make_op_test "Add - fractions add up to 1" add (1, 2) (1,2) (1,1);
     make_op_test "Add - fractions add up to whole number > 1" add (1, 2)
       (3, 2) (2, 1);
@@ -26,7 +30,32 @@ let rationals_tests =
     make_op_test "Add - adding fractions > 1" add (42, 5) (21, 5) (63, 5);
     make_op_test "Add - +/-, sum positive" add (1, 2) (-1, 4) (1, 4);
     make_op_test "Add - +/-, sum negative" add (1,4) (-1,2) (1,-4);
-    make_op_test "Add - sum of negatives" add (-1,4) (-1,3) (-7,12)
+    make_op_test "Add - sum of negatives" add (-1,4) (-1,3) (-7,12);
+    make_op_test "Add - negative whole num" add (-1,4) (-3,4) (-1,1);
+    make_op_test "Add - negative wohle num < -1" add (-3,2) (-1,2) (-2,1);
+
+    make_op_test "Mul - 0 * 0 = 0" mul (0,1) (0,2) (0,1);
+    make_op_test "Mul - x * 0 = 0" mul (1,1) (0,1) (0,1);
+    make_op_test "Mul - x * 1 = x" mul (1,1) (3,5) (3,5);
+    make_op_test "Mul - fractions" mul (1,2) (1,4) (1,8);
+    make_op_test "Mul - fractions > 1" mul (3,2) (5,2) (15,4);
+    make_op_test "Mul - whole numbers" mul (4,2) (3,1) (6,1);
+    make_op_test "Mul - +/- numerators" mul (-1,2) (1,4) (1,-8);
+    make_op_test "Mul - +/- denominators" mul (1,2) (1,-2) (1,-4);
+    make_op_test "Mul - -/- -> +" mul (-1,2) (-1,2) (1,4);
+
+    make_op_test "Div - x / x = 1" div (1,2) (1,2) (1,1);
+    make_op_test "Div - x / 1 = x" div (1,2) (1,1) (1,2);
+    make_op_test "Div - whole #s" div (4,1) (2,1) (2,1);
+    make_op_test "Div - +/+ fractions" div (1,4) (1,2) (1,2);
+    make_op_test "Div - +/- -> -" div (1,4) (-1,2) (1,-2);
+    make_op_test "Div - -/- -> +" div (-1,4) (1,-2) (1,2);
+
+    make_op_test "Sub - x - 0 = x" sub (1,2) (0,1) (1,2);
+    make_op_test "Sub - x - y > 0" sub (1,2) (1,4) (1,4);
+    make_op_test "Sub - x - y < 0" sub (1,4) (1,2) (1,-4);
+    make_op_test "Sub --x - y < 0" sub (-1,4) (1,2) (-3,4);
+    make_op_test "Sub - subtracting negative" sub (1,4) (-1,2) (3,4);
   ]
 
 let suite = "test suite for LinAlg" >::: List.flatten [
