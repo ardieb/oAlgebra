29dca2cfc81393164ac6633d82df08ffb30bce0a
amb556 <amb556@cornell.edu>
2019-04-16 09:42:42 -0400

    Added float to rationals

--- a6-src/rationals.ml
+++ a6-src/rationals.ml
@@ -1,35 +1,39 @@
 open Matrix
 type rational = 
   | Int of int
-  | Frac of int * int 
+  | Frac of int * int  
+  | Float of float
 module RATIONAL : NUM with type t = rational = struct
-  (** A module for working with t numbers *)
+  (** A module for working with t rationalbers *)
   exception ArithmeticError
-
-  (* AF: a value of type [t] is an int * int pair where the first integer
-   * represents the numerator and the second integer represents the denominator *)
-  (* RI: the denominator of the t may not be equal to zero and the denominator may not be negative *)
+  (* AF: A value of type t is a Int of type int, a Frac of type int * int,
+   * where the first number is the numerator, and the second number is the
+   * denominator, or a Float of type float *)
+  (* RI: The denominator of a Frac may not be zero *)
   type t = rational
-  (** [zero] is the zero repsentation of a rational number *)
+  (** [zero] is [Int 0] *)
   let zero = Int 0
-  (** [one] is the one representation of a rational number *)
+  (** [one] is [Int 1] *)
   let one = Int 1
-  (** [rep_ok r] fails if r is not a valid t number, does nothing otherwise *)
+  (** [rep_ok r] is the rational number if the reprsentation meets the R.I. and
+    * fails otherwise *)
   let rep_ok = function
     | Int i -> Int i
+    | Float f -> Float f
     | Frac (n,d) -> begin
         if d = 0 then raise ArithmeticError
         else if d < 0 then Frac (-n, -d)
         else Frac (n,d)
       end
-  (** [gcd n1 n2] is the largest number that divides n1 and n2 evenly *)
+  (** [gcd n1 n2] is the largest integer that divides n1 and n2 with no remainder *)
   let rec gcd = fun (n1:int) (n2:int) ->
     if n2 = 0 then n1
     else gcd n2 (n1 mod n2) 
-  (** [simplify (n,d)] is the simplified t number using the greatest
+  (** [simplify (n,d)] is the simplified t rational using the greatest
     * common divisor of [n] and [d] *)
   let simplify = function
     | Int i -> Int i
+    | Float f -> Float f
     | Frac (n,d) -> begin
         if d = 0 then raise ArithmeticError else
           let d' = gcd n d in
@@ -39,7 +43,12 @@ module RATIONAL : NUM with type t = rational = struct
           else if d < 0 then Frac (-n, -d)
           else Frac (n,d)
       end
-  (** [add (n1,d1) (n2,d2)] is the sum of two t numbers *)
+  (** [to_float (n,d)] is the float representation of a rational number *)
+  let to_float = function
+    | Int i -> float_of_int i
+    | Float f -> f
+    | Frac (n,d) -> (float_of_int n) /. (float_of_int d)
+  (** [add (n1,d1) (n2,d2)] is the sum of two rational numbers *)
   let rec add = fun (r1:t) (r2:t) ->
     match rep_ok r1, rep_ok r2 with
     | Int i1, Int i2 -> Int (i1 + i2)
@@ -52,40 +61,52 @@ module RATIONAL : NUM with type t = rational = struct
         simplify (Frac ((n + i1),d))
       end
     | Frac (n1,d1), Frac (n2,d2) -> simplify (Frac (((n1*d2) + (n2*d1)),(d1*d2)))
-  (** [mul (n1,d1) (n2,d2)] is the product of two t numbers *)
-  let mul = fun (r1:t) (r2:t) ->
+    | Float f1, Float f2 -> Float (f1 +. f2)
+    | Float f, b -> add (Float f) (Float (to_float b))
+    | a, Float f -> add (Float (to_float a)) (Float f)
+  (** [mul (n1,d1) (n2,d2)] is the product of two t rational numbers *)
+  let rec mul = fun (r1:t) (r2:t) ->
     match rep_ok r1, rep_ok r2 with
     | Int i1, Int i2 -> Int (i1 * i2)
     | Int i, Frac (n,d) -> simplify (Frac ((i*n),d))
     | Frac (n,d), Int i -> simplify (Frac ((i*n),d))
     | Frac (n1,d1), Frac (n2,d2) -> simplify (Frac ((n1*n2),(d1*d2)))
-  (** [div (n1.d1) (n2.d2)] is the quotient of r1 by r2 *)
-  let div = fun (r1:t) (r2:t) ->
+    | Float f1, Float f2 -> Float (f1 *. f2)
+    | Float f, b -> mul (Float f) (Float (to_float b))
+    | a, Float f -> mul (Float (to_float a)) (Float f)
+  (** [div (n1.d1) (n2.d2)] is the quotient of [r1] by [r2] *)
+  let rec div = fun (r1:t) (r2:t) ->
     match rep_ok r1, rep_ok r2 with
     | Int i1, Int i2 -> mul r1 (Frac (1,i2))
     | Int i, Frac (n,d) -> mul r1 (Frac (d,n))
     | Frac (n,d), Int i -> mul (Frac (n,d)) (Frac (1,i))
     | Frac (n1,d1), Frac (n2,d2) -> mul (Frac (n1,d1)) (Frac (d2,n2))
-  (** [sub (n1,d1) (n2,d2)] is the difference of two t numbers *)
-  let sub = fun (r1:t) (r2:t) ->
+    | Float f1, Float f2 -> Float (f1 /. f2)
+    | Float f, b -> div (Float f) (Float (to_float b))
+    | a, Float f -> div (Float (to_float a)) (Float f)
+  (** [sub (n1,d1) (n2,d2)] is the difference of two rational numbers *)
+  let rec sub = fun (r1:t) (r2:t) ->
     match rep_ok r1, rep_ok r2 with
     | Int i1, Int i2 -> Int (i1 - i2)
     | Int i, Frac (n,d) -> add r1 (Frac (-n,d))
     | Frac (n,d), Int i -> add r1 (Int (-i))
     | Frac (n1,d1), Frac (n2,d2) -> add r1 (Frac (-n2,d2))
-  (** [to_float (n,d)] is the float representation of a t number *)
-  let to_float = function
-    | Int i -> float_of_int i
-    | Frac (n,d) -> (float_of_int n) /. (float_of_int d)
-  (** [to_string (n,d)] is the string repsentation of a t number *)
+    | Float f1, Float f2 -> Float (f1 -. f2)
+    | Float f, b -> sub (Float f) (Float (to_float b))
+    | a, Float f -> sub (Float (to_float a)) (Float f)
+  (** [to_string (n,d)] is the string repsentation of a t rationalber *)
   let to_string = function
     | Int i -> string_of_int i
+    | Float f -> begin
+      let d,n = modf f in
+      string_of_float (n +. floor (d *. 1000.0))
+    end
     | Frac (n,d) -> (string_of_int n)^"/"^(string_of_int d)
-  (** [compare f1 f2] is the order of the rational numbers f1 and f2.
-    * If f1 is less than f2, is LT
-    * If f1 is greater than f2, is GT
-    * If f1 is equal to f2, is EQ *)
-  let compare = fun (r1:t) (r2:t) -> 
+  (** [compare r1 r2] is the order of the rational numbers [r1] and [r2]
+    * If [r1] is less than [r2], is LT
+    * If [r1] is greater than [r2], is GT
+    * If [r1] is equal to [r2], is EQ *)
+  let rec compare = fun (r1:t) (r2:t) -> 
     match rep_ok r1, rep_ok r2 with
     | Int i1, Int i2 -> begin
         if i1 > i2 then GT
@@ -111,13 +132,21 @@ module RATIONAL : NUM with type t = rational = struct
         else if (n1 mod d1) > (n2 mod d2) then GT
         else EQ
       end
+    | Float f1, Float f2 ->
+      if f1 < f2 then LT
+      else if f1 > f2 then GT
+      else EQ
+    | Float f, b -> compare (Float f) (Float (to_float b))
+    | a, Float f -> compare (Float (to_float a)) (Float f)
   (** [abs f] is the absolute value of the rational number [f] *)
   let abs = function
     | Int i -> Int (Pervasives.abs i)
+    | Float f -> Float (Pervasives.abs_float f)
     | Frac (n,d) -> Frac (Pervasives.abs n, Pervasives.abs d)
   (** [neg f] is the negation of [f] *)
   let neg = function
     | Int i -> Int (-i)
+    | Float f -> Float (-.f)
     | Frac (n,d) -> Frac (-n,d)
   (** [format fmt f] is the formatted version of [f] using [fmt] *)
   let format = fun (fmt:Format.formatter) (f:t) -> 
--- a6-src/rationals.mli
+++ a6-src/rationals.mli
@@ -2,4 +2,5 @@ open Matrix
 type rational =
   | Int of int
   | Frac of int * int
+  | Float of float
 module RATIONAL : NUM with type t = rational
